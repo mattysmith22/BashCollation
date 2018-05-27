0b61180431a8b9896c5f21587c98b771bf6efc8a
Matthew Smith <mattysmith22@googlemail.com>
2018-05-28 00:27:37 +0100

    Moved exporting the data to another method Added basic parameter parsing - missing some functions

--- collate.sh
+++ collate.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-configfilename="$1"
-
-rundir="$(pwd)" #The directory that it was written 
-indexdir="$(dirname "$(realpath "$1")")" #The directory that the index file is in
-tempdir="$(mktemp -d)" #The temporary directory
-
-outputFile="$(pwd)/Output.pdf" #The file to output the PDF to.
-
 # return the name of the file entered, without directory or file extension
 
 getFileName ()
@@ -39,31 +31,95 @@ appendFile ()
 {
 	if [ ! -e "$outputFile" ]; then
 		mv "$1" "$outputFile" 
-		echo "first file"
 	else
 		gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOUTPUTFILE="${tempdir}/OutTemp.pdf" "$outputFile" "$1" > /dev/null
 		mv "${tempdir}/OutTemp.pdf" "$outputFile"
 	fi
 }
 
-rm "$outputFile"
-cd "$indexdir"
-pwd
-while read line
-do
-	filename="$(basename -- "$line")"
-	fileextension="$(getFileExt "${line}")"
-	if [[ "$fileextension" = "odt" ]] || [[ "$fileextension" = "ods" ]]; then
-		printf "Adding %s (libreoffice export) ... " "$line"
-		exportLibreoffice "$line"
-		printf "done\n"
-	elif [[ "$fileextension" = "pdf" ]]; then
-		printf "Adding %s (pdf append) ... " "$line"
-		appendFile "$line"
-		printf "done\n"
-	else
-		printf "Error: Don't know how to process .%s files, cannot export %s\n" "$fileextension" "$filename"
-	fi
-done < "$(realpath "$configfilename")"
+exportIndex ()
+{
+	configfilename="$1"
 
-rm -r "${tempdir}"
+	rundir="$(pwd)" #The directory that it was written 
+	indexdir="$(dirname "$(realpath "$1")")" #The directory that the index file is in
+	indexfile="$(realpath "$configfilename")" #The absolute path to the index file
+	tempdir="$(mktemp -d)" #The temporary directory
+
+	outputFile="$(realpath "$2")" #The file to output the PDF to.
+
+	rm "$outputFile"
+	cd "$indexdir"
+	while read line
+	do
+		filename="$(basename -- "$line")"
+		fileextension="$(getFileExt "${line}")"
+		if [[ "$fileextension" = "odt" ]] || [[ "$fileextension" = "ods" ]]; then
+			printf "Adding %s (libreoffice export) ... " "$line"
+			exportLibreoffice "$line"
+			printf "done\n"
+		elif [[ "$fileextension" = "pdf" ]]; then
+			printf "Adding %s (pdf append) ... " "$line"
+			appendFile "$line"
+			printf "done\n"
+		else
+			printf "Error: Don't know how to process .%s files, cannot export %s\n" "$fileextension" "$filename"
+		fi
+	done < "${indexfile}"
+
+	rm -r "${tempdir}"
+}
+
+printHelp ()
+{
+	echo "USAGE:"
+	echo ""
+	echo "collate init <index>"
+	echo "  creates an index file at the given location. The file name must also be provided."
+	echo ""
+	echo "collate add <index> <filepath>"
+	echo "  add the file at <filepath> to the <index> file."
+	echo ""
+	echo "collate add <index> <filepath> <num>"
+	echo "  add the file at <filepath> to the <index> file. It is placed at line number <num>."
+	echo ""
+	echo "collate clear <index>"
+	echo "  clears the <index> of all file paths"
+	echo ""
+	echo "collate exec <index>"
+	echo "  runs the index file at <index>. Output file is the default, Output.pdf in the working directory"
+	echo ""
+	echo "collate exec <index> <output>"
+	echo "  runs the index file at <index>. Output file is <output>."
+	
+}
+
+if [ $# -eq 0 ]; then
+    echo "Please enter a valid commmand"
+	echo ""
+	printHelp
+	exit 0
+elif [[ $1 = "init" ]] && [[ $# -eq 2 ]]; then
+    echo "Init not yet implemented"
+	exit 1
+elif [[ $1 = "add" ]] && { [[ $# -eq 2 ]] || [[ $# -eq 3 ]]; }; then
+    echo "Add not yet implemented"
+	exit 1
+elif [[ $1 = "clear" ]] && [[ $# -eq 2 ]]; then
+    echo "Clear not yet implemented"
+	exit 1
+elif [[ $1 = "exec" ]] && { [[ $# -eq 2 ]] || [[ $# -eq 3 ]]; }; then
+    if [[ $# -eq 3 ]]; then
+	    exportIndex "$2" "$3"
+	else
+	    exportIndex "$2" "Output.pdf"
+	fi
+	exit 0
+elif [[ $1 = "help" ]]; then
+    printHelp
+else
+    echo "Please enter a valid commmand"
+	echo ""
+	printHelp
+	exit 0 
+fi
